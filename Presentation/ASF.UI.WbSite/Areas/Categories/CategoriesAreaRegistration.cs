9b909213b6df3991b5635eb064da2f6a5662e1e2
gtrioullier <gustavo.trioullier@alumnos.uai.edu.ar>
2017-10-03 21:22:48 -0300

    not-working-dev-victor

---
+++ Presentation/ASF.UI.WbSite/Areas/Categories/CategoriesAreaRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Areas.Categories
+{
+    public class CategoriesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName => "Categories";
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(null, "categorias/delete/{id}", new { action = "Delete", controller = "Category" });
+            context.MapRoute(null, "categorias/edit/{id}", new { action = "Edit", controller = "Category", id = UrlParameter.Optional });
+            context.MapRoute(null, "categorias/add", new { action = "Edit", controller = "Category" });
+            context.MapRoute(null, "categorias/{id}", new { action = "Detail", controller = "Category" });
+            context.MapRoute(null, "categorias", new { action = "Index", controller = "Category" });
+        }
+    }
+}
---
+++ Presentation/ASF.UI.WbSite/Areas/Categories/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASF.Framework.Localization.Kernel.Interfaces.Services;
+using ASF.Framework.Localization.Kernel.Interfaces.Services.UnitOfWork;
+using ASF.UI.Process;
+
+namespace ASF.UI.WbSite.Areas.Categories.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        public CategoryController(IUnitOfWorkManager unitOfWorkManager,
+            ILocalizationService localizationService,
+            ISettingsService settingsService)
+            
+        {
+            
+        }
+        // GET: Categories/Category
+        public ActionResult Index()
+        {
+            var cpf = new CategoryProcess();
+            var data = cpf.Find(2);
+
+            var cp = new CategoryProcess();
+            var lista = cp.SelectList();
+            return View(lista);
+        }
+    }
+}
